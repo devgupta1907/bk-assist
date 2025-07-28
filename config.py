52178e59767af2c165d13f70ee9d66eb1bbbdc41
Dev Gupta <devgupta1907@gmail.com>
2025-07-28 15:27:15 +0530

    added st secrets

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
 from dotenv import load_dotenv
+import streamlit as st
 
 def load_config():
     load_dotenv()
     return {
         "chroma_collection": "faq_collection",
-        "gemini_api_key": os.getenv("GEMINI_API_KEY"),
+        "gemini_api_key": st.secrets["GEMINI_API_KEY"]
     }
